c3e64002f5ca79ca67171579590e71dba4ae500d
RitchielRez <ritchielrez@gmail.com>
2021-11-19 22:15:11 +0600

    Now specifing Cmake toolchain file works

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <fmt/core.h>
 
+// This program uses the fmt library, a really useful library for formatting your strings. It's really fast,
+// also used by a lot of popular apps, such as aesprite(Pixel image editor), Windows Terminal, MongoDB etc.
+
 int main() {
-    fmt::print("Hello {1}{0}{1}", "World!", "\n");
+    // Here, we're using positional arguments to format strings like C#
+    std::string s = fmt::format("Hello {1}{0}{1}", "World!", "\n");
+    
+    fmt::print("{}", s);
+
+    return 0;
 }
