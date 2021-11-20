04243c10d690209478b67c84052d707741786d4f
RitchielRez <ritchielrez@gmail.com>
2021-11-20 15:50:16 +0600

    Code action works with C++ in neovim

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 // This program uses the fmt library, a really useful library for formatting your strings. It's really fast,
 // also used by a lot of popular apps, such as aesprite(Pixel image editor), Windows Terminal, MongoDB etc.
 
-int main() {
-    // Here, we're using positional arguments to format strings like C#
-    std::string s = fmt::format("Hello {1}{0}{1}", "World!", "\n");
-    
-    fmt::print("{}", s);
+using fmt::format;
+using fmt::print;
+using std::string;
 
-    return 0;
+int main() {
+  // Here, we're using positional arguments to format strings like C#
+  string s = format("Hello {1}{0}{1}", "World!", "\n");
+
+  print("{}", s);
+
+  return 0;
 }
